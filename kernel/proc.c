e8095a351026c45db135b2f7fae7aa8dee4f3cf3
zhuxinyu <898656123@qq.com>
2024-09-27 16:19:16 +0800

    speed syscall

--- kernel/proc.c
+++ kernel/proc.c
@@ -132,6 +132,13 @@ found:
     return 0;
   }
 
+  if ((p->us = (struct usyscall *)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->us->pid = p->pid;
+
   // An empty user page table.
   p->pagetable = proc_pagetable(p);
   if(p->pagetable == 0){
@@ -157,6 +164,8 @@ freeproc(struct proc *p)
 {
   if(p->trapframe)
     kfree((void*)p->trapframe);
+  if(p->us)
+    kfree((void*)p->us);
   p->trapframe = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
@@ -198,6 +207,15 @@ proc_pagetable(struct proc *p)
   if(mappages(pagetable, TRAPFRAME, PGSIZE,
               (uint64)(p->trapframe), PTE_R | PTE_W) < 0){
     uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
+  // map pid
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->us), PTE_R) < 0) {
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
     uvmfree(pagetable, 0);
     return 0;
   }
